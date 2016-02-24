2244c58d54b0476d15dc3b68bba9ef96e5c45d94
mehmetunluu <mehmetunluu@gmail.com>
2016-02-24 17:23:33 +0200

    filter mapping eklendi.

---
+++ cmweb/cmweb-servlet/src/main/java/org/cmweb/log/LogFilter.java
@@ -0,0 +1,22 @@
+package org.cmweb.log;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by M.UNLU on 24.02.2016.
+ */
+public class LogFilter implements Filter {
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("ip adresi "+servletRequest.getRemoteAddr());
+    }
+
+    public void destroy() {
+
+    }
+}
